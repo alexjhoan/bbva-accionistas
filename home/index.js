654d5a277b883ce10434a1ddb36a6903e23d96d5
alexjhoan <alexjhoan.25@gmail.com>
2022-09-17 09:14:33 -0400

    init commit

---
+++ home/index.js
@@ -0,0 +1,3 @@
+$('div[data-component="header"]').load("components/header.html")
+$('div[data-component="login"]').load("components/login.html")
+$('div[data-component="footer"]').load("components/footer.html")

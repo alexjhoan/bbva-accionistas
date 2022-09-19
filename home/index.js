9ddfc9544d1fad0ac2e21ff87c9fd6ec8fe1f055
alexjhoan <alexjhoan.25@gmail.com>
2022-09-19 11:05:35 -0400

    fix singin

--- home/index.js
+++ home/index.js
@@ -1,3 +1,12 @@
 $('div[data-component="header"]').load("components/header.html")
 $('div[data-component="login"]').load("components/login.html")
 $('div[data-component="footer"]').load("components/footer.html")
+
+$(window).on("load", function () {
+  const tooltipTriggerList = document.querySelectorAll(
+    '[data-bs-toggle="tooltip"]'
+  )
+  const tooltipList = [...tooltipTriggerList].map(
+    (tooltipTriggerEl) => new bootstrap.Tooltip(tooltipTriggerEl)
+  )
+})
